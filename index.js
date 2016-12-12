c2a7f28fbaa5fc91c530424e7bc4e19fc0182adf
ChenHaifeng <m@cheft.cn>
2016-12-12 18:55:27 +0800

    fix #1 #2 #3

--- index.js
+++ index.js
@@ -7,16 +7,19 @@ module.exports = function(content) {
   }
   
   dot.templateSettings.selfcontained = true;
+  dot.templateSettings.varname = 'data'
 
   var content = fs.readFileSync(this.resourcePath).toString();
-
-  if (content.indexOf('<script>') !== -1) {
-  	var arr = content.split('<script>');
-  	var html = arr[0];
-  	var js = arr[1];
-  	js = js.replace('</script>', '').replace('module.exports = {', 'module.exports = {\ntpl: ' + dot.template(html) + ',');
-  	return js;
+  
+  var regexp = /<script>([\w|\W]*?)<\/script>/;
+  var matchs = regexp.exec(content);
+  if (!matchs) {
+    return "module.exports = " + dot.template(content);
+  } else if (matchs[1].trim() == '') {
+    var html = content.replace(regexp, '');
+    return 'module.exports = {tpl: ' + dot.template(html) + '};';
   } else {
-  	return "module.exports = " + dot.template(content);
+    var html = content.replace(regexp, '');
+    return matchs[1].replace('<script>', '').replace('module.exports = {', 'module.exports = {\n\ttpl: ' + dot.template(html) + ',').replace('</script>', '');
   }
 };
